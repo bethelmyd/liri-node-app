27496a340209002bc347bdfcf3c7379523894f52
bethelmyd <bethelmyd@gmail.com>
2016-11-26 00:26:55 -0500

    working on putting together the different modules

--- liri.js
+++ liri.js
@@ -3,36 +3,50 @@ var Twit = require('twitter');
 
 var twitKeys = require ('./keys.js');
 
-console.log(twitKeys.twitterKeys.consumer_key);
+var fs = require("fs");
+var path = "./log.txt";
 
-var t = new Twit(twitKeys.twitterKeys);
+function twitter(){
+    var t = new Twit(twitKeys.twitterKeys);
 
-var params = {
-    q: 'banana since:2011-11-11',
-    count: 10
-};
+    var params = {
+        q: 'banana since:2011-11-11',
+        count: 10
+    };
 
-t.get('search/tweets', params, gotData);
+    t.get('search/tweets', params, gotData);
 
-function gotData(err, data, response)
-{
-    for(var i = 0; i < data.statuses.length; i++)
-        console.log(data.statuses[i].text);
+    function gotData(err, data, response)
+    {
+        for(var i = 0; i < data.statuses.length; i++)
+        {
+            console.log(data.statuses[i].text);
+            fs.appendFile(path, data.statuses[i].text+"\n", function(error) {
+                if (error) {
+                    console.error("write error:  " + error.message);
+                } else {
+                    console.log("Successful Write to " + path);
+                }
+            });
+        }
+    }
 }
 
 
 
+function spotify(){
+    var spot = require('spotify');
+    
+    spot.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+    
+        // Do something with 'data'
+        console.log(data);
+    });
+}
 
 
-// var spotify = require('spotify');
- 
-// spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-//     if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
- 
-//     // Do something with 'data'
-//     console.log(data);
-// });
 
